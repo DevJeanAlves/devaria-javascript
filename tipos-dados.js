3ce42766e5b521dafffbd20dbc00acac98edf25c
JeanAlves2021 <jean.pdolsky2@gmail.com>
2021-04-27 20:56:58 -0300

    inputs js

---
+++ tipos-dados.js
@@ -0,0 +1,19 @@
+const inteiro = 10;
+const decimal = 10.5;
+const bool = false;
+const nome = 'jean';
+let copo = null;
+let televisao;
+let pessoa = {
+    nome: 'jean'
+}
+const symb = Symbol()
+
+console.log(typeof inteiro);
+console.log(typeof decimal);
+console.log(typeof bool);
+console.log(typeof nome);
+console.log(typeof copo);
+console.log(typeof televisao);
+console.log(typeof pessoa);
+console.log(typeof symb);
---
+++ variaveis-constantes.js
@@ -0,0 +1,5 @@
+var cor = 'branca';
+let nome = 'Jean';
+const empresa = 'Devaria'
+
+console.log(cor, nome, empresa);
